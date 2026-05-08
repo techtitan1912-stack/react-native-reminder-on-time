971c6dc4ba9b98d1429a39c1cef803b5064620f1
techtitan1912-stack <techtitan1912@gmail.com>
2026-05-09 01:49:15 +0530

    firebase env setup

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,5 +1,9 @@
 import admin from "firebase-admin";
-import serviceAccount from "../config/serviceAccountKey.json" with { type: "json" };
+// import serviceAccount from "../config/serviceAccountKey.json" with { type: "json" };
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
