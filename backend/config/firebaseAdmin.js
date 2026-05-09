47afd509a917af8d17c466855747e1714a8adc74
techtitan1912-stack <techtitan1912@gmail.com>
2026-05-09 22:50:45 +0530

    backend all error solve

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,12 +1,18 @@
 import admin from "firebase-admin";
 // import serviceAccount from "../config/serviceAccountKey.json" with { type: "json" };
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT
-);
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+
+  console.log("Firebase Admin initialized");
+}
 
 export default admin;
